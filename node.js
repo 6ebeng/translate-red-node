2d3dd46235e43a842e7826520d8e942eee7819c6
r3kbin <rekbin.devs@gmail.com>
2022-08-27 20:02:40 +0300

    Add batch request support

--- node.js
+++ node.js
@@ -9,17 +9,19 @@ module.exports = function (RED) {
         this.on('input', function (msg) {
             msg.config = conf;
             var conf = { to: config.to };
-            var phrase = msg.payload + '';
+            var input = msg.payload + '';
             if (config.from === 'prog' && config.to === 'prog') {
                 conf = { from: msg.payload.from, to: msg.payload.to }
             } else if (config.from === 'auto' && config.to === 'prog') {
                 conf = { to: msg.payload.to }
             }
-            if (msg.payload.phrase) {
-                phrase = msg.payload.phrase;
+            if (msg.payload.input) {
+                input = msg.payload.input;
+            } else {
+                input = '';
             }
-            translate(phrase + '', conf).then(function (res) {
-                msg.payload = res.text;
+            await translate(input, conf).then(function (res) {
+                msg.payload = res;
                 node.send(msg);
             }).catch(function (err) {
                 node.error(err);
